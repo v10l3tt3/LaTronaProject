96af3a2d49d459d5804fe83fb977301d8c639cea
v10l3tt3 <viomule@icloud.com>
2025-05-06 23:32:30 +0200

    esqueleto Menu-Creditos-Scripts

---
+++ Assets/UI Recursos/UI Scripts/AudioManager.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioClip bandaSonora;
+    public AudioClip fxButtonIn;
+    public AudioClip fxButtonOut;
+    public AudioClip fxPlayButton;
+    public AudioClip fxExitGame;
+
+    public AudioClip fxPiece1;
+    public AudioClip fxPiece2;
+    public AudioClip fxPiece3;
+
+    public AudioClip fxPhotoCompleted;
+
+    public GameObject musicObj;
+
+    public static AudioManager Instance;
+    AudioSource gestorAudio;
+    AudioSource audioMusic;
+
+    //por si se quiere usar un mixer de audio al entrar en una zona:
+    //public AudioMixerSnapshot burbujaSnapshot;
+
+    void Awake()
+    {
+        //Si existe otro objeto de este tipo, destruyelo
+        if (Instance != null && Instance != this){
+            Destroy(Instance.gameObject);
+        }else{
+            Instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+
+    void Start()
+    {
+        gestorAudio = this.GetComponent<AudioSource>();
+        
+
+        audioMusic = musicObj.GetComponent<AudioSource>();
+        audioMusic.clip = bandaSonora;
+        audioMusic.loop = true;
+        audioMusic.volume = 0.1f;
+        audioMusic.Play();
+    }
+
+   
+    void Update()
+    {
+        
+    }
+
+    //Metodo seleccionador de clips de audio
+    public void SonarClipUnaVez(AudioClip ac){
+        gestorAudio.PlayOneShot(ac, 1f);
+    }
+}
---
+++ Assets/UI Recursos/UI Scripts/MenuUI.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuUI : MonoBehaviour
+{
+    GameObject mainPanel;
+    GameObject settingsPanel;
+    void Start()
+    {
+        settingsPanel = GameObject.Find("SettingsPanel");
+        settingsPanel.SetActive(false);
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("____");
+    }
+
+    public void ShowSettings()
+    {
+        AudioManager.Instance.SonarClipUnaVez(AudioManager.Instance.fxButtonIn);
+        settingsPanel.SetActive(true);
+    }
+
+    public void HideSettings()
+    {
+        AudioManager.Instance.SonarClipUnaVez(AudioManager.Instance.fxButtonOut);
+        settingsPanel.SetActive(false);
+    }
+
+    public void ExitGame()
+    {
+        AudioManager.Instance.SonarClipUnaVez(AudioManager.Instance.fxExitGame);
+        Application.Quit();
+    }
+
+    public void ButtonSound(){
+        AudioManager.Instance.SonarClipUnaVez(AudioManager.Instance.fxPlayButton);
+    }
+
+}
