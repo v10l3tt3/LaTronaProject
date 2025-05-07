2a99b40b069a43fc6857ca0b8c5019c0fd346f25
v10l3tt3 <viomule@icloud.com>
2025-05-07 14:02:59 +0200

    texto de creditos revisado y corregido

--- Assets/UI Recursos/UI Scripts/CreditsUI.cs
+++ Assets/UI Recursos/UI Scripts/CreditsUI.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class CreditsUI : MonoBehaviour
 {
-    // Start is called before the first frame update
+    
     void Start()
     {
-        
+
+      
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         
