0803311514a71d16d2c81dc5bfb04af07213eab6
v10l3tt3 <viomule@icloud.com>
2025-05-08 21:41:02 +0200

    UIsliced16x16 pre-slicedinteriors botonscript

---
+++ Assets/Scripts/BotonesInventarioScript.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BotonesInventarioScript : MonoBehaviour
+{
+    GameObject mainPanel;
+    GameObject cablesPanel;
+    GameObject tuberiasPanel;
+    GameObject cochePanel;
+
+    void Start()
+    {
+        cablesPanel = GameObject.Find("CablesPanel");
+        cablesPanel.SetActive(false);
+        tuberiasPanel = GameObject.Find("TuberiasPanel");
+        tuberiasPanel.SetActive(false);
+        cochePanel = GameObject.Find("CochePanel");
+        cochePanel.SetActive(false);
+    }
+    
+    void Update()
+    {
+        
+    }
+
+
+    //funciones para botones generales
+    public void ReturnMenu()
+    {
+        AudioManager.Instance.SonarClipUnaVez(AudioManager.Instance.fxButtonOut);
+        SceneManager.LoadScene("MenuUI");
+    }
+
+    public void ExitGame()
+    {
+        AudioManager.Instance.SonarClipUnaVez(AudioManager.Instance.fxExitGame);
+        Application.Quit();
+    }
+
+
+
+
+    //funciones para iniciar paneles de minijuegos
+    //CABLES
+    public void ShowCables()
+    {
+        AudioManager.Instance.SonarClipUnaVez(AudioManager.Instance.fxButtonIn);
+        cablesPanel.SetActive(true);
+    }
+    public void HideCables()
+    {
+        AudioManager.Instance.SonarClipUnaVez(AudioManager.Instance.fxButtonOut);
+        cablesPanel.SetActive(false);
+    }
+
+
+    //TUBERIAS
+    public void ShowTuberias()
+    {
+        AudioManager.Instance.SonarClipUnaVez(AudioManager.Instance.fxButtonIn);
+        tuberiasPanel.SetActive(true);
+    }
+
+    public void HideTuberias()
+    {
+        AudioManager.Instance.SonarClipUnaVez(AudioManager.Instance.fxButtonOut);
+        tuberiasPanel.SetActive(false);
+    }
+
+
+    //COCHE
+    public void ShowCoche()
+    {
+        AudioManager.Instance.SonarClipUnaVez(AudioManager.Instance.fxButtonIn);
+        cochePanel.SetActive(true);
+    }
+    public void HideCoche()
+    {
+        AudioManager.Instance.SonarClipUnaVez(AudioManager.Instance.fxButtonOut);
+        cochePanel.SetActive(false);
+    }
+}
