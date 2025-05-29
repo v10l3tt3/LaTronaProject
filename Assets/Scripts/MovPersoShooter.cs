b8afb63692de32cb05d9246a178f332fdedcdfcd
v10l3tt3 <viomule@icloud.com>
2025-05-29 15:04:29 +0200

    inventario updated+iconos items en setpanelfalse

--- Assets/Scripts/MovPersoShooter.cs
+++ Assets/Scripts/MovPersoShooter.cs
@@ -2,9 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+using TMPro;
+using System;
+
+
 public class MovPersoShooter : MonoBehaviour
 {
-    
+
     public float velocidad = 5f;
     public float multiplicador = 5f;
     float movTeclas;
@@ -13,6 +18,19 @@ public class MovPersoShooter : MonoBehaviour
     private Vector2 moveInput;
 
     public bool miraDerecha = true;
+
+
+    //ACTIVAR INDICACIONES DIALOGO 
+    /*[SerializeField] private GameObject dialogueMark;
+    [SerializeField] private GameObject dialogoPanel;
+    [SerializeField] private TMP_Text dialogueText;
+    [SerializeField, TextArea(4, 6)] private string[] dialogueLines;
+
+    public float typingTime = 0.05f;
+
+    private bool isPlayerInRange;
+    private bool didDialogueStart;
+    private int lineIndex;*/
     void Start()
     {
         playerRb = this.GetComponent<Rigidbody2D>();
@@ -34,7 +52,7 @@ public class MovPersoShooter : MonoBehaviour
              0,
              0
         );
-        
+
         //Animacion IDLE TO WALKING
         if (movTeclas != 0)
         {
@@ -44,12 +62,15 @@ public class MovPersoShooter : MonoBehaviour
         {
             animatorController.SetBool("WalkH", false);
         }
-        
+
         //Aprox2 de FLIP(ambas direcciones)
-        if(movTeclas < 0){
+        if (movTeclas < 0)
+        {
             this.GetComponent<SpriteRenderer>().flipX = true;
             miraDerecha = false;
-        }else if(movTeclas > 0){
+        }
+        else if (movTeclas > 0)
+        {
             this.GetComponent<SpriteRenderer>().flipX = false;
             miraDerecha = true;
         }
@@ -58,6 +79,36 @@ public class MovPersoShooter : MonoBehaviour
     void Update()
     {
         float miDeltaTime = Time.deltaTime;
-        
     }
+        //Acabar si da tiempo: typing apprering indiccations
+        /*
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+            StartDialogue();
+
+
+        }
+    }
+
+    public void StartDialogue()
+    {
+        didDialogueStart = true;
+        dialogoPanel.SetActive(true);
+        //dialogueMark.SetActive(false);
+        lineIndex = 0;
+        //Time.timeScale = 0f; // Pausa todo el juego
+        StartCoroutine(ShowDialogueLine());
+    }
+
+    private IEnumerator ShowDialogueLine()
+    {
+        dialogueText.text = string.Empty;
+        foreach(char ch in dialogueLines[lineIndex])
+        {
+            dialogueText.text += ch;
+            yield return new WaitForSecondsRealtime(typingTime);
+        }
+    }*/
+
 }
