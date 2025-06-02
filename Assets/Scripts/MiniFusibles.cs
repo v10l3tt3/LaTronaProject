b86ca17f81d08431f5c265d4ee3341b70996590b
v10l3tt3 <viomule@icloud.com>
2025-06-02 20:42:18 +0200

    luces ya se apagan

--- Assets/Scripts/MiniFusibles.cs
+++ Assets/Scripts/MiniFusibles.cs
@@ -64,16 +64,16 @@ public class MiniFusibles : MonoBehaviour
         fo1 = GameObject.Find("Plomillos-orange-1");
         fo2 = GameObject.Find("Plomillos-orange-2");
 
-        light0 = GameObject.Find("Light2D0");
+        /*light0 = GameObject.Find("Light2D0");
         light1 = GameObject.Find("Light2D1");
         light2 = GameObject.Find("Light2D2");
         light3 = GameObject.Find("Light2D3");
         light4 = GameObject.Find("Light2D4");
         light5 = GameObject.Find("Light2D5");
-        light6 = GameObject.Find("Light2D6");
+        light6 = GameObject.Find("Light2D6");*/
 
 
-        
+
         fb1.GetComponent<BoxCollider2D>().enabled = true;
         fb2.GetComponent<BoxCollider2D>().enabled = true;
         fb3.GetComponent<BoxCollider2D>().enabled = true;
@@ -82,13 +82,14 @@ public class MiniFusibles : MonoBehaviour
         fo1.GetComponent<BoxCollider2D>().enabled = true;
         fo2.GetComponent<BoxCollider2D>().enabled = true;
 
-        light0.GetComponent<Light2D>().enabled = false;
-        light1.GetComponent<Light2D>().enabled = false;
-        light2.GetComponent<Light2D>().enabled = false;
-        light3.GetComponent<Light2D>().enabled = false;
-        light4.GetComponent<Light2D>().enabled = false;
-        light5.GetComponent<Light2D>().enabled = false;
-        light6.GetComponent<Light2D>().enabled = false;
+        /*light0.SetActive(false);
+        light1.SetActive(false);
+        light2.SetActive(false);
+        light3.SetActive(false);
+        light4.SetActive(false);
+        light5.SetActive(false);
+        light6.SetActive(false);*/
+        
 
         
         //tinyLight.SetActive(false);
